567a2b4cd6cb4da8d54a720931212afd7985580c
xhevdet <xhevdet.kryeziu@kujtesa.com>
2021-07-21 16:53:26 +0200

    first commit

---
+++ components/styles.js
@@ -0,0 +1,58 @@
+
+import styled from 'styled-components/native';
+
+import { View, Image, Text } from 'react-native';
+import Constants from 'expo-constants'
+
+const StatusBarHeight = Constants.statusBarHeight;
+
+// colors
+export const Colors = {
+  primary: '#ffffff',
+  secondary: '#E5E7EB',
+  tertiary: '#1F2937',
+  darkLight: '#9CA3AF',
+  brand: '#6D28D9',
+  green: '#10B981',
+  red: '#EF4444',
+};
+
+const {primary, secondary, tertiary, darkLight, brand, green, red} = Colors;
+
+export const StyledContainer = styled.View`
+  flex: 1;
+  padding: 25px;
+  padding-top: ${StatusBarHeight + 30}px;
+  background-color: ${primary};
+`
+
+export const InnerContainer = styled.View`
+  width: 100%;
+  flex: 1;
+  align-items: center;
+`;
+
+export const PageLogo = styled.Image`
+  width: 250px;
+  height: 200px;
+`;
+
+export const PageTitle = styled.Text`
+  font-size: 30px;
+  text-align: center;
+  font-weight: bold;
+  color: ${brand};
+  padding: 10px;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: 18px;
+  margin-bottom: 20px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: ${tertiary};
+`;
+
+export const StyledFormArea = styled.View`
+  width: 90%;
+`;
