fb8dc48ef7c0dba439f73654f95647d48fbef13e
xhevdet20 <xhevdet1996@gail.com>
2021-07-21 23:33:11 +0200

    new part 2

--- screens/Login.js
+++ screens/Login.js
@@ -1,22 +1,96 @@
-import React from 'react'
+import React, { useState, useContext } from 'react';
 import { StatusBar } from 'expo-status-bar';
+
+// formik
+import { Formik } from 'formik';
+
 import {
   StyledContainer,
-  InnerContainer,
   PageLogo,
-  PageTitle
-} from '../components/styles'
+  PageTitle,
+  SubTitle,
+  StyledInputLabel,
+  StyledFormArea,
+  StyledButton,
+  StyledTextInput,
+  LeftIcon,
+  RightIcon,
+  InnerContainer,
+  ButtonText,
+  Colors,
+} from './../components/styles';
+import { View, ActivityIndicator } from 'react-native';
+
+//colors
+const { darkLight, brand, primary } = Colors;
+
+// icon
+import { Octicons, Fontisto, Ionicons } from '@expo/vector-icons';
+
 
 const Login = () => {
-  return(
-    <StyledContainer>
-      <StatusBar style="dark" />
-      <InnerContainer>
-        <PageLogo resizeMode="cover" source={require('./../assets/img/expo-bg1.png')}/>
-        <PageTitle>Flower Crib</PageTitle>
-      </InnerContainer>
-    </StyledContainer>
+
+
+  // Persisting login
+  const persistLogin = (credentials, message, status) => {
+    AsyncStorage.setItem('flowerCribCredentials', JSON.stringify(credentials))
+      .then(() => {
+        handleMessage(message, status);
+        setStoredCredentials(credentials);
+      })
+      .catch((error) => {
+        handleMessage('Persisting login failed');
+        console.log(error);
+      });
+  };
+
+  return (
+
+      <StyledContainer>
+        <StatusBar style="dark" />
+        <InnerContainer>
+          <PageLogo resizeMode="cover" source={require('./../assets/img/expo-bg1.png')} />
+          <PageTitle>Flower Crib</PageTitle>
+          <SubTitle>Account Login</SubTitle>
+
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            onSubmit={(values) => {
+            console.log(values);
+            }}
+          >
+            {({ handleChange, handleBlur, handleSubmit, values, }) => (
+              <StyledFormArea>
+                <MyTextInput
+                  label="Email Address"
+                  placeholder="andyj@gmail.com"
+                  placeholderTextColor={darkLight}
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                  keyboardType="email-address"
+                  icon="mail"
+                />
+                
+              </StyledFormArea>
+            )}
+          </Formik>
+        </InnerContainer>
+      </StyledContainer>
   );
-}
+};
+
+const MyTextInput = ({ label, icon, ...props }) => {
+  return (
+    <View>
+      <LeftIcon>
+        <Octicons name={icon} size={30} color={brand} />
+      </LeftIcon>
+      <StyledInputLabel>{label}</StyledInputLabel>
+      <StyledTextInput {...props} />
+  
+    </View>
+  );
+};
 
 export default Login;
